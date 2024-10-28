7a0ce35b5b6c0261384e7203519453c449640d86
Name <email@example.ru>
2024-10-29 01:17:34 +0400

    29.7. Промежуточная сдача проекта (HW-03)

---
+++ LifeSpot/Static/JS/About.js
@@ -0,0 +1,16 @@
+﻿let userData = new Map();
+function writeUserData() {
+    //alert('dfdg');
+    let sesDate = new Date();
+    userData.set('name', prompt("Введите ваше имя"));
+    userData.set('comment', prompt("Введите текст"));
+    userData.set('date', sesDate.toLocaleString());
+}
+
+const addComment = () => {
+    //for (let i of userData) { console.log(i) };
+    let elem = document.getElementsByClassName('input-container')[0];
+    console.log(elem.innerHTML);
+    elem.innerHTML += '<p><b>' + userData.get('name') + ' ' + userData.get('date') + '</b></p>';
+    elem.innerHTML += '<p>' + userData.get('comment') + '</p>';
+}
---
+++ LifeSpot/Static/JS/Index.js
@@ -0,0 +1,50 @@
+﻿let userData = new Map();
+
+setTimeout(function () {alert("Подпишись на мой инстаграм myinsta.com !")}, 60000);
+
+function checkAge() {
+    
+    userData.set('age', prompt("Введите ваш возраст"))
+ 
+    if (userData.get('age') >= 18) {
+        alert("Приветствуем на LifeSpot " + new Date().toLocaleString());
+    }
+    else {
+        alert("Трансляции предназначены для лиц моложе 18 лет. Вы будете перенаправлены.");
+        window.location.href = "http://www.google.com"
+    }
+
+}
+
+function handleSession() {
+    let sesDate = new Date();
+    userData.set('sesDate', sesDate.getHours() + ':' + sesDate.getMinutes() + ' ' + sesDate.getSeconds());
+    userData.set('browser', window.navigator.userAgent);
+}
+
+
+let sessionLog = function () {
+    for (let i of userData) { console.log(i) };
+}
+
+
+/*Фильтрация содержимого страницы. Код ниже отрабатывает каждый раз при вводе символа в input*/
+// Получаем содержимое по тегу. Сохраняем текст пользовательского ввода      
+function filtrVideo()
+{
+    //let txt = document.getElementsByTagName('input')[0].value;
+    // Получаем объект по классу видео-контейнер
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        // Получаем содержимое по тегу h3 внутри видеоконтейнера
+        let blName = (elements[i].getElementsByTagName('h3')[0].innerHTML);
+
+        // Проверем вмещает ли тект в блоке введённый тект из поля ввода. Делаем блок видимым если есть совпадения, либо наоборот
+        // если совпадений нет
+        if (blName.toLowerCase().includes(userInput().toLowerCase())) { elements[i].style.display = 'inline-block' }
+        else { elements[i].style.display = 'none' };
+
+    }
+
+}
